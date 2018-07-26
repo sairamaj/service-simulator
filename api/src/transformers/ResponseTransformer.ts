ab25a31c429fa0093633b22f88f171b4e33b791e
sairamaj <sairamaj@gmail.com>
2018-07-25 20:57:15 -0700

    formatting date has been fixed

--- api/src/transformers/ResponseTransformer.ts
+++ api/src/transformers/ResponseTransformer.ts
@@ -5,15 +5,23 @@ import * as dateformat from 'dateformat'
 export class ResponseTransformer {
 
     constructor() {
-        handlebars.registerHelper('date', function (format, days) {
-            console.log('>>>>>in date')
-            var date = new Date();
-            var newDate = date.setDate(date.getDate());
-            var formattedDate = dateformat(newDate, 'yyyy-MM-dd')
-            console.log('>>> formated date:' + formattedDate)
-            return formattedDate;
-        });
 
+        handlebars.registerHelper('date', function (context) {
+
+            var format = 'yyyy-MM-dd'
+            if( context.hash["format"]){
+                format = context.hash['format']
+            }
+            var days = 0
+            if( context.hash['days']){
+                days = +context.hash['days']
+            }
+       
+            var date = new Date();
+            var newDate = date.setDate(date.getDate() + days);
+            var formattedDate = dateformat(newDate, format)
+            return formattedDate;    
+        });
     }
 
     public async transform(data: string): Promise<string> {
