dc518121053dacc08d9881922c9c8ef8aec5c228
sairamaj <sairamaj@gmail.com>
2018-08-05 12:02:46 -0700

    manual5 was added

--- azure/manual5.bat
+++ azure/manual5.bat
@@ -2,5 +2,10 @@ echo off
 echo -----------------------------------------------------------
 echo             Createing registry
 echo -----------------------------------------------------------
-rem az container create --name simtesting --image servicesimulatorregistry.azurecr.io/testing/simulator:latest --dns-name-label servicesimulator --registry-username <usernamehere> --registry-password <pwdhere>
+rem az cosmosdb list --name coaspsimulator
+rem az container create --name simtesting --image servicesimulatorregistry.azurecr.io/testing/simulator:latest --dns-name-label servicesimulator --registry-username <usernamehere> --registry-password <pwdhere> 
+rem az container create -g ServiceSimulator --name simtesting --image servicesimulatorregistry.azurecr.io/testing/simulator:latest --dns-name-label servicesimulator  --registry-user <username> --registry-password <pwd> -e PROVIDER=mongo MONGODB_CONNECTION="mongodbconnectionstring"
+rem append database name (simulator before ?ssl=true ,i.e xxxxx/simulator?ssl=true)
+
 echo  after the above command you can browse at http://servicesimulator.eastus.azurecontainer.io/
+
