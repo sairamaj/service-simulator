4184dc1473278554e35deb3f00d5ac6a5eafede8
sairama jamalapuram <sairamaj@gmail.com>
2018-08-13 10:11:39 -0700

    documentation in progress

--- buildall.bat
+++ buildall.bat
@@ -2,9 +2,11 @@ REM convert this in to gulp
 echo off
 echo "Building Api"
 pushd api
+call npm install
 call npm run-script build
 popd
 pushd dashboard
 echo "Building dashboard"
+call npm install
 call npm run-script build
 popd
